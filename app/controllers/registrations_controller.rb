67e17be3bac4072872d7fb9a6888a72627fff072
Bach Nghia Nguyen <bachnghianguyen@gmail.com>
2021-09-23 11:27:34 +0700

    Login with session cookies

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,12 +6,15 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
     if (@user.save)
+      session[:user_id] = @user.id
       redirect_to root_path, notice: "Create account successfully"
     else
       render :new
     end
     # render plain: params[:user]
   end
+
+  private
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
